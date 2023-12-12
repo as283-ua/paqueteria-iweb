2e4cfada9a092c4b1e0da10286415b73f10c20ba
Andrejs Sikorskis <as283@alu.ua.es>
2023-12-12 17:34:23 +0100

    Extendido megatest

--- src/main/java/iwebpaqueteria/model/Direccion.java
+++ src/main/java/iwebpaqueteria/model/Direccion.java
@@ -3,6 +3,7 @@ package iwebpaqueteria.model;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -42,4 +43,97 @@ public class Direccion {
 
     @OneToMany(mappedBy = "direccionDestino")
     private Set<Envio> enviosDestino;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(int planta) {
+        this.planta = planta;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Set<Envio> getEnviosOrigen() {
+        return enviosOrigen;
+    }
+
+    public Set<Envio> getEnviosDestino() {
+        return enviosDestino;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Direccion direccion = (Direccion) o;
+        return numero == direccion.numero && planta == direccion.planta && Objects.equals(id, direccion.id) && Objects.equals(codigoPostal, direccion.codigoPostal) && Objects.equals(localidad, direccion.localidad) && Objects.equals(provincia, direccion.provincia) && Objects.equals(calle, direccion.calle) && Objects.equals(telefono, direccion.telefono);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigoPostal, localidad, provincia, numero, planta, calle, telefono);
+    }
 }
