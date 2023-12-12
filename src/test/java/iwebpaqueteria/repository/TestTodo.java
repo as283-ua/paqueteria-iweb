b4181d11eb0e5ff379771e650108f9a48378afd2
Andrejs Sikorskis <as283@alu.ua.es>
2023-12-12 17:20:09 +0100

    Test rol

--- src/test/java/iwebpaqueteria/repository/TestTodo.java
+++ src/test/java/iwebpaqueteria/repository/TestTodo.java
@@ -1,5 +1,6 @@
 package iwebpaqueteria.repository;
 
+import iwebpaqueteria.model.Rol;
 import iwebpaqueteria.model.Usuario;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Sql(scripts = "/clean-db.sql")
 public class TestTodo {
 
     @Autowired
@@ -21,14 +21,30 @@ public class TestTodo {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private RolRepository rolRepository;
+
     @Test
     @Transactional
     public void absolutamenteTodoTest(){
+        Rol rol = new Rol();
+        rol.setNombre("Administrador");
+        rol = rolRepository.save(rol);
+
         // Crear un usuario
         Usuario usuario = new Usuario();
         usuario.setNombre("Juan");
         usuario.setEmail("usu@ua");
         usuario.setContrasenya("1234");
         usuario.setTelefono("123456789");
+        usuario.setRol(rol);
+
+        usuario = usuarioRepository.save(usuario);
+
+        usuario = usuarioRepository.findById(usuario.getId()).orElse(null);
+
+        assertThat(usuario).isNotNull();
+
+        assertThat(usuario.getRol().getNombre()).isEqualTo("Administrador");
     }
 }
