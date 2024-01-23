536c3034ce1164e870a30f91cf2f66a41f866bb7
Gonzalo Zapata <gmzl1@alu.ua.es>
2024-01-23 17:37:46 +0100

    detalles tienda funcionando

--- src/main/java/iwebpaqueteria/controller/TiendaController.java
+++ src/main/java/iwebpaqueteria/controller/TiendaController.java
@@ -2,7 +2,11 @@ package iwebpaqueteria.controller;
 import iwebpaqueteria.authentication.ManagerUserSession;
 import iwebpaqueteria.controller.exception.UsuarioNoLogeadoException;
 import iwebpaqueteria.controller.exception.UsuarioSinPermisosException;
+import iwebpaqueteria.dto.DireccionData;
+import iwebpaqueteria.dto.EnvioData;
 import iwebpaqueteria.dto.UsuarioData;
+import iwebpaqueteria.service.DireccionService;
+import iwebpaqueteria.service.EnvioService;
 import iwebpaqueteria.service.RolService;
 import iwebpaqueteria.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Controller
@@ -21,6 +28,10 @@ public class TiendaController {
     ManagerUserSession managerUserSession;
     @Autowired
     RolService rolService;
+    @Autowired
+    EnvioService envioService;
+    @Autowired
+    DireccionService direccionService;
     @GetMapping("/tiendas/{userId}/APIKEY")
     public String mostrarApiKey(@PathVariable(value = "userId") Long idTienda, Model model, HttpSession session) {
         comprobarUsuarioLogeado();
@@ -73,6 +84,7 @@ public class TiendaController {
         Set<UsuarioData> tiendas = rolService.listarUsuariosPorRol("tienda");
         model.addAttribute("tiendas", tiendas);
 
+
         return "listadoTiendas";
     }
 
@@ -86,4 +98,25 @@ public class TiendaController {
         return "redirect:/tiendas";
     }
 
+    public Map<Long, DireccionData> direccionesDeEnvios(List<EnvioData> envios){
+        Map<Long, DireccionData> result = new HashMap<>();
+
+        for (EnvioData envio : envios) {
+            result.put(envio.getId(), direccionService.findById(envio.getDireccionDestinoId()));
+        }
+
+        return result;
+    }
+    @GetMapping("/tiendas/{id}")
+    public String detalleTienda(@PathVariable(value="id") Long idUsu,Model model, HttpSession session){
+        comprobarUsuarioLogeadoWebMaster();
+
+        if (usuarioService.findById(idUsu) == null)
+            return "redirect:/tiendas";
+
+        model.addAttribute("tienda",usuarioService.findById(idUsu));
+        model.addAttribute("envios",envioService.enviosTienda(idUsu,null));
+        model.addAttribute("direcciones", direccionesDeEnvios(envioService.enviosTienda(idUsu,null)));
+        return "detalleTienda";
+    }
 }
